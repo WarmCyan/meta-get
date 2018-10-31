4fe9638b8c15591e057d7cbf0a26980cf7259882
Alec West <alecwest38@gmail.com>
2018-10-31 13:42:14 -0500

    Move pip into api/pkg_managers

--- meta/tests/test_api_pkg_managers_pip.py
+++ meta/tests/test_api_pkg_managers_pip.py
@@ -1,4 +1,4 @@
-# meta.tests.test_api_pip.py
+# meta.tests.test_api_pkg_managers_pip.py
 
 # Copyright (C) 2018 Nathan Martindale <nathanamartindale@gmail.com>
 # Licensed under GPL-3.0: https://www.gnu.org/licenses/gpl-3.0.en.html
@@ -10,7 +10,7 @@
 
 import pytest
 
-import meta.api.pip
+import meta.api.pkg_managers.pip
 
 # --------------------------------------
 #   Fixtures
@@ -32,16 +32,16 @@ def shell_mock(mocker):
 
 def test_default_command_execution(shell_mock):
     """Ensure that a command gets passed correctly to the underlying shell function."""
-    meta.api.pip.execute("freeze")
+    meta.api.pkg_managers.pip.execute("freeze")
     shell_mock.assert_called_with("pip freeze", silent=False)
 
 
 def test_silent_command_execution(shell_mock):
     """Ensure that the api, if called silently, calls the backend shell silently."""
-    meta.api.pip.execute("freeze", silent=True)
+    meta.api.pkg_managers.pip.execute("freeze", silent=True)
     shell_mock.assert_called_with("pip freeze", silent=True)
 
 
 def test_command_execution_return(shell_mock):
     """Ensure that the shell output gets returned correctly."""
-    assert meta.api.pip.execute("freeze") == "meta-get==0.1.0"
+    assert meta.api.pkg_managers.pip.execute("freeze") == "meta-get==0.1.0"
