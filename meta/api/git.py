4055a51e9abb5faee3cabad98d2a17190d1275c3
Nathan Martindale <nathanamartindale@gmail.com>
2019-01-13 14:45:40 -0600

    Add git API clone implementation

--- meta/api/git.py
+++ meta/api/git.py
@@ -7,8 +7,36 @@
 
 import logging
 
+from meta.api.filesystem import Folder
+import meta.current
 import meta.shell
 
 
 def clone(url, destination=None, silent=False):
-    pass
+    """Clones the desired github repo locally."""
+
+    logging.info("Git API requested to clone %s", url)
+
+    # figure out what the result folder would be based on url
+    # (in other words, just get the repo name)
+    stripped_url = url
+    if url[-4:] == ".git":
+        stripped_url = url[:-4]
+    name = stripped_url[stripped_url.rfind("/") + 1 :]
+
+    # determine where to clone the repo
+    dest_path = meta.current.REPO_DIR + "/" + name
+    if destination is not None:
+        dest_path = destination
+        name = destination[destination.rfind("/") + 1 :]
+
+    logging.info("Git API cloning into %s", destination)
+
+    meta.shell.execute("git clone {0} {1}".format(url, destination), silent)
+
+    # add to autotracker
+    meta.current.PACKAGE_AUTOTRACKER.files.append(dest_path)
+
+    # return a folder of the new local_repo
+    local_repo = Folder(dest_path)
+    return local_repo
